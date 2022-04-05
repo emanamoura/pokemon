57170c436ff217ffe94b2e1de52ba02576fc4204
emanamoura <emanuel.moura@ccc.ufcg.edu.br>
2022-04-05 17:42:20 -0300

    Container that holds all cards

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,19 @@
+import './style.scss';
+import { Card } from "../../components/Card";
+import { useEffect } from 'react';
+
+
+export function Home() {
+    const pokemonsId = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100];
+    return(
+        <main className='home'>
+            {pokemonsId.map((pokemonId) => {
+                return <Card pokemonId={pokemonId}/>
+            })}
+        </main>
+    )
+}
+
+function useState<T>(arg0: never[]): [any, any] {
+    throw new Error('Function not implemented.');
+}
